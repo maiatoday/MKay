46f9ead2c9ccbbd50252811b8b82772da154773d
maiatoday <maiatoday@gmail.com>
2017-09-20 13:07:16 +0200

    LIcense list

--- app/src/main/java/net/maiatoday/mkay/ui/MainActivity.kt
+++ app/src/main/java/net/maiatoday/mkay/ui/MainActivity.kt
@@ -4,8 +4,17 @@ import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 import net.maiatoday.mkay.R
+import android.widget.Toast
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
+import android.content.Intent
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -37,5 +46,27 @@ class MainActivity : AppCompatActivity() {
 
         viewModel = ViewModelProviders.of(this).get(EntriesViewModel::class.java)
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.getItemId()) {
+            R.id.acction_licenses -> {
+                showLicenses()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showLicenses() {
+        val intent = Intent(this, OssLicensesMenuActivity::class.java)
+        val title = getString(R.string.title_activity_licenses)
+        intent.putExtra("title", title)
+        startActivity(intent)
     }
 }
